38a66d05c651c1e767e3bb916557584ace04a194
jose8manuel <elchepo21_@hotmail.com>
2017-11-23 16:08:22 -0700

    Listado, Print to PDF

--- listado/main.js
+++ listado/main.js
@@ -3,6 +3,17 @@
 const {app,BrowserWindow} = require('electron');
 const path = require('path')//Muestra la ruta del archivo
 const url = require('url');//Carga una página
+//Constantes para pdf
+const electron = require('electron');
+//Sistema de archivos
+const fs = require('fs');
+//Acceso al sistema operativo
+const os = require('os');
+//Para declarar una funcion remota
+const ipc = electron.ipcMain;
+//Acceso a la terminal-línea de comandos
+const shell = electron.shell;
+
 //ECMASCRIPT = 6
 let PantallaPrincipal;
 //Objeto global para compartir datos
@@ -26,8 +37,23 @@ function muestraPantallaPrincipal(){
 	PantallaPrincipal.show();
 }
 
+//Evento para PDF (declaración)
+ipc.on('print-to-pdf',function(event){
+	const pdfPath=path.join(os.tmpdir(),'print.pdf')
+	const win=BrowserWindow.fromWebContents(event.sender)
+	win.webContents.printToPDF({},function(error,data){
+		if(error) throw error
+		fs.writeFile(pdfPath,data,function(error){
+			if(error){
+			throw error
+		}
+		shell.openExternal('file://'+pdfPath)
+		win.close()
+		})	
+
+	})
+});
+
 app.on('ready',muestraPantallaPrincipal)
-
-
 
 
