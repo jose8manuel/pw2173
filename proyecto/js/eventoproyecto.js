38e99df7d7e9fc1e74da7270525e57487eae1bc3
jose8manuel <elchepo21_@hotmail.com>
2017-12-04 16:16:01 -0700

    proyectophpfin

---
+++ proyecto/js/eventoproyecto.js
@@ -0,0 +1,31 @@
+var inicia = function(){
+    var entrada = function(){
+    	var usuario = $("#txtUsuario").val();
+    	var clave   = $("#txtClave").val();
+    	var parametros="opc=valida"+
+    				   "&usuario="+usuario+
+    				   "&clave="+clave+
+    				   "&id="+Math.random();
+    	$.ajax({
+    		url:"php/entrada.php",
+    		dateType: 'json', //retorno
+            type: "POST", //lo que enviamos
+            data:parametros,
+    		success: function(data){
+                if(data.respuesta == true){
+                    alert("Bienvenido");
+                }else{
+                    alert("Usuario y/o clave incorrectos");
+                }
+
+    		},
+    		error: function(a,b,c){
+                alert("No se pudo conectar al server")
+
+    		}
+    	});
+    }
+
+	$("#btnEntrar").on("click",entrada)
+}
+$(document).ready(inicia);
